765d6656e0f62700c3e4986023d9649226db166c
Saud Faisal <sa3oouoodi@gmail.com>
2026-09-02 20:25:50 +0300

    added positive and negative and Zero

--- Odd-Even-Checker/Odd-even-Checker.py
+++ Odd-Even-Checker/Odd-even-Checker.py
@@ -1,14 +1,31 @@
 #Start
 # input number
+# if numb == 0 
+# numb = 0 
+# if numb postive or negative
 # if % == 0 
 # Even number
 # else 
 # Odd number 
 #End
 
+
+
+
 numb = int(input("Enter a number: "))
 
-if numb % 2 == 0:
-    print(f"The number {numb} is even.")
+
+
+if numb == 0 :
+    print("Your number is Zero")
+elif numb > 0:
+    print("Your number is positive")
 else:
-    print(f"The number {numb} is odd.")
+    print("Your number is Negative")
+
+if numb ==0:
+    print("")
+elif numb % 2 == 0 :
+    print(f"The number {numb} is even")
+else:
+    print(f"The number {numb} is odd")
