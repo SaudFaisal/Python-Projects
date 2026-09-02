15be17d54c49b71719a14405679901b0f20ce7c3
Saud Faisal <sa3oouoodi@gmail.com>
2026-09-02 19:35:50 +0300

    Simple code diff even or odd

---
+++ Odd-Even-Checker/Odd-even-Checker.py
@@ -0,0 +1,14 @@
+#Start
+# input number
+# if % == 0 
+# Even number
+# else 
+# Odd number 
+#End
+
+numb = int(input("Enter a number: "))
+
+if numb % 2 == 0:
+    print(f"The number {numb} is even.")
+else:
+    print(f"The number {numb} is odd.")
