e7b748f7f1ec86a708f57c9544bae8310c7d4d2f
Saud Faisal <sa3oouoodi@gmail.com>
2026-09-10 18:15:58 +0300

    added +,-,*,/ and ValueError function and options with quit (need to add some details for while so any error dont repeat whole code) for calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,12 +1,14 @@
-#create get_number for Value error and setup quit option as str first then make it int
-#use while true so it be on a loop until the user wants to quit
-#input num1 , num2 , oper
-#let user chose from 1-4
-#1 = add , 2 = subtract , 3 = multiply , 4 = divide
-#make sure to use if for None for the get_number and if =="quit" so he can quit the code
-#use if statement to check whether the user choose from if = 1 the do add and so one using elif
-#make sure no logical error like divide by zero fix it by if statement
 
+'''
+1.create get_number for Value error and setup quit option as str first then make it int
+2.use while true so it be on a loop until the user wants to quit
+3.input num1 , num2 , oper
+4.let user chose from 1-4
+4. 1 = add , 2 = subtract , 3 = multiply , 4 = divide
+5.make sure to use if for None for the get_number and if =="quit" so he can quit the code
+6.use if statement to check whether the user choose from if = 1 the do add and so one using elif
+7.make sure no logical error like divide by zero fix it by if statement
+'''
 
 
 
@@ -32,20 +34,17 @@ while True:
         continue
     if num1 == "quit":
         break
-
-
-while True:
-    num2 = get_number("Enter the Second number To quit type q or quit: ")
-    if num2 is None:
-        continue
-    if num2 == "quit":
-            break
-while True:
     oper = get_number("Enter which operator \n1 - Addition\n2 - Subtraction\n3 - multiplication\n4 - division\nTo quit type q or quit \nEnter your opition here: ")
     if oper is None:
         continue
     if oper == "quit":
             break
+    num2 = get_number("Enter the Second number To quit type q or quit: ")
+    if num2 is None:
+        continue
+    if num2 == "quit":
+            break
+
     if oper == 1:
         print(num1 + num2)
     elif oper == 2:
