7d406cf7b63e1434471e405dd7e44bdcde934657
Saud Faisal <sa3oouoodi@gmail.com>
2026-09-11 20:16:14 +0300

    edit Pesudocode to To-Do-List.py

--- To-Do-List/To-Do-List.py
+++ To-Do-List/To-Do-List.py
@@ -1,15 +1,17 @@
-#create get_number to avoid ValueError
-#create an empty list
-
-
-
-
-
-
-
-
-
-
+'''
+1.create get_number to avoid ValueError
+2.create an empty list[]
+3.create a loop while True so that code keep playing until the user wants to quit
+4.choice = input 1,2,3,4
+5.1 = add task , 2 = delete task , 3 = show tasks , 4 = Quit
+6.add task use task.append(input()) so that the user enter a task and the task stores it in the list[]
+7.delete task user len(task)=0 to know if there is any task stored if no then continue from the beginning
+8.use for with 2 variable and enumerate(task): so that is shows what is inside the list with the Index of it 
+9.deltask = the number of Index wanted to delete , use len(task) > deltask so that if the user type an Index not in the list it prevent this from happening
+10.del task[deltask] this delete the task in the list remember deltast = Index 
+11.show tasks we use if len(task) == 0 if no task print no task , we use for and enumerate(task) to print the Index and the task in the list
+12.Quit we print GoodBye! and break the loop
+'''
 
 def get_number(text):
      try:
@@ -19,8 +21,6 @@ def get_number(text):
           print("That's not a number,Try again")
           return None
         
-
-
 
 task = []
 
